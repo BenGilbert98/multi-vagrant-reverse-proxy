19022ed80bc8d9c668d28ba2aab977c52acd2086
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-29 22:19:25 +0000

    Reverse Proxy

---
+++ README.md
@@ -0,0 +1,24 @@
+# Multi Vagrant Machine Task (Reverse Proxy)
+
+## Intro
+This repo is for a multi vagrant machine which will run on port 80.
+
+## Pre-requisites
+- Install Vagrant
+- Install VirtualBox
+
+## Instructions
+To work in this box:
+1. Clone the repo into your chosen location
+2. Inside bash navigate to the location where the cloned repo is and type the following command
+
+```bash
+vagrant up
+```
+3. The code in the provisions.sh file should mean that this is the only command you will need to run
+
+### Website locations
+The website will run on the following urls
+- 192.168.10.100
+- 192.168.10.100/fibonacci/{your chosen number}
+- 192.168.10.100/posts
---
+++ environment/app/provision.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Update the sources list
+sudo apt-get update -y
+
+# install git
+sudo apt-get install git -y
+
+
+# #read changes just made to bash rc using current process not child
+# source ~/.bashrc
+
+# install nodejs
+sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install pm2
+sudo npm install pm2 -g
+
+sudo apt-get install nginx -y
+
+# finally, restart the nginx service so the new config takes hold
+sudo rm /etc/nginx/sites-available/default
+sudo cp /home/ubuntu/environment/port80.conf /etc/nginx/sites-available/default
+sudo systemctl restart nginx.service
+
+#make changes to .bashrc
+echo 'export DB_HOST=192.168.10.200' >> ~/.bashrc
+
+# Starts app
+cd /home/ubuntu/app
+sudo pm2 start app.js --update-env
